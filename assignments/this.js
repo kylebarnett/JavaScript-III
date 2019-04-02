0c839c350058024e1920585d362592f1f8e0f4eb
Kyle Barnett <kbarnett23@gmail.com>
2019-04-02 15:32:02 -0600

    principle 3 done

--- assignments/this.js
+++ assignments/this.js
@@ -10,6 +10,15 @@
 */
 
 // Principle 1
+function sayName(name) {
+  console.log(this);
+  return name;
+}
+console.log(sayName("Kyle"));
+
+// code example for Window Binding
+
+// Principle 2
 const obj1 = {
   name: "Kyle",
   age: 23,
@@ -19,14 +28,20 @@ const obj1 = {
 }
 console.log(obj1.interests())
 
-// code example for Window Binding
-
-// Principle 2
-
 // code example for Implicit Binding
 
 // Principle 3
+function Skills(javascript, html, css){
+  this.javascript = 'Javascript',
+  this.html = 'HTML',
+  this.css = 'CSS',
+  this.combine = function(){
+    return `My skills are ${this.javascript}, ${this.html}, ${this.css}`
+  }
+}
 
+const javascript = new Skills()
+console.log(javascript.combine())
 // code example for New Binding
 
 // Principle 4
